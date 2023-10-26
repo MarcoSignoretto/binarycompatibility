c0a3c48901ec0e99df44b81c9764d05496c631fb
Marco Signoretto <marco.signoretto.dev@gmail.com>
2023-10-26 07:32:15 +0400

    remove commented line

--- lib/src/main/kotlin/com/msignoretto/binarycompatibility/lib/DishesRepository.kt
+++ lib/src/main/kotlin/com/msignoretto/binarycompatibility/lib/DishesRepository.kt
@@ -1,6 +1,5 @@
 package com.msignoretto.binarycompatibility.lib
 
 public interface DishesRepository {
-//    public fun getDishes(specification: DishSpecification = DishSpecifications.all()): List<Dish>
     public fun getDishes(): List<Dish>
 }
